5a4d0516162d1be39f2e1b53de15d600ad1a76a5
Sujal-Rabari <134353926+Sujal-Rabari@users.noreply.github.com>
2024-02-20 12:25:09 -0500

    My Second Commit

---
+++ src/Sujal_Rabari.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Sujal Rabari
+ */
+public class Sujal_Rabari {
+    
+}
+
+/* Hey It's Sujal Rabari here,
+this is my branch and it's java file
+thank you.
+*/
